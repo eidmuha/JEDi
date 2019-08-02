429d140e2bc9c675bf892febec206f7aab5ff7fe
Dannar Mawardi <dmawardi@gmail.com>
2019-08-02 20:35:40 +1000

    Cleaned

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -6,40 +6,49 @@ var chatBoard = $('#chatBoard');
 var messageBox = $('#chatInput');
 var messageButton = $('#sendMessage');
 
-// var commentsRef = database.ref('post-comments/' + postId);
 
-database.ref('/'+restaurantName).on('child_added', function(snap) {
-  console.log(snap.val());
+// When page is loaded
+$(document).ready(function(){
 
-  var newMessage = $('<p>');
-  newMessage.text(snap.val().message);
 
-  chatBoard.append(newMessage);
+
+    // Event listener
+    messageButton.on('click', function(event){
+        // Prevent default form submission behavior
+        event.preventDefault();
+    
+        database.ref('/'+restaurantName).push({
+            // Push the message
+            message: messageBox.val(),
+            // & a timestamp for ordering later
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+          }, function(error) {
+            if (error) {
+              // The write failed...
+              console.log(error);
+            } else {
+              // Data saved successfully!
+              console.log('Message sent successfully!');
+    
+            }
+          });
+
+
+        //   Database listeners
+        database.ref('/'+restaurantName).on('child_added', function(snap) {
+            console.log(snap.val());
+          
+            var newMessage = $('<p>');
+            newMessage.text(snap.val().message);
+          
+            chatBoard.append(newMessage);
+          });
+          
+          
+          database.ref('/'+restaurantName).on('value', function(snap) {
+              console.log(snap.val());
+          });
+    })
+
+
 });
-
-// commentsRef.on('child_changed', function(data) {
-// });
-
-database.ref().on('child_removed', function(snap) {
-    console.log(snap.val());
-});
-
-messageButton.on('click', function(event){
-    // Prevent default form submission behavior
-    event.preventDefault();
-
-    database.ref('/'+restaurantName).push({
-        // Push the message
-        message: messageBox.val(),
-        // & a timestamp for ordering later
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      }, function(error) {
-        if (error) {
-          // The write failed...
-          console.log(error);
-        } else {
-          // Data saved successfully!
-          console.log('Message sent successfully!')
-        }
-      });
-})
