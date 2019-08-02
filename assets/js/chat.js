7891d4478b6d083a141f4a7e48d7fe14c9a7880c
Dannar Mawardi <dmawardi@gmail.com>
2019-08-02 21:14:57 +1000

    Added renderChat function

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,52 +2,74 @@ var database = firebase.database();
 
 var restaurantName = 'Blue Barracudas';
 
-var chatBoard = $('#chatBoard');
+var chatBoard = $('#messageBoard');
 var messageBox = $('#chatInput');
 var messageButton = $('#sendMessage');
 
+// Function List
+// 
+// Renders CHat
+function RenderChat(snap) {
+    console.log('key' + snap.ref.key)
+    var newMessage = $('<p>');
+    newMessage.text(snap.val().message);
+
+    chatBoard.append(newMessage);
+
+}
+
 
 // When page is loaded
-$(document).ready(function(){
+$(document).ready(function () {
 
+    // Arguments begin here
+    console.log('starting up');
+    var ref = database.ref('/' + restaurantName).orderByChild('dateAdded');
 
+    ref.once('value', function (snap) {
+        console.log(snap.val());
+        RenderChat(snap);
+    });
 
     // Event listener
-    messageButton.on('click', function(event){
+    messageButton.on('click', function (event) {
         // Prevent default form submission behavior
         event.preventDefault();
-    
-        database.ref('/'+restaurantName).push({
+
+
+
+        database.ref('/' + restaurantName).push({
             // Push the message
             message: messageBox.val(),
             // & a timestamp for ordering later
             dateAdded: firebase.database.ServerValue.TIMESTAMP
-          }, function(error) {
+        }, function (error) {
             if (error) {
-              // The write failed...
-              console.log(error);
+                // The write failed...
+                console.log(error);
             } else {
-              // Data saved successfully!
-              console.log('Message sent successfully!');
-    
+                // Data saved successfully!
+                console.log('Message sent successfully!');
+
             }
-          });
+        });
 
 
         //   Database listeners
-        database.ref('/'+restaurantName).on('child_added', function(snap) {
+        database.ref('/' + restaurantName).on('child_added', function (snap) {
             console.log(snap.val());
-          
-            var newMessage = $('<p>');
-            newMessage.text(snap.val().message);
-          
-            chatBoard.append(newMessage);
-          });
-          
-          
-          database.ref('/'+restaurantName).on('value', function(snap) {
-              console.log(snap.val());
-          });
+            console.log('child added');
+
+            // var newMessage = $('<p>');
+            // newMessage.text(snap.val().message);
+            // chatBoard.append(newMessage);
+            RenderChat(snap);
+        });
+
+
+        //   database.ref('/'+restaurantName).on('value', function(snap) {
+        //       console.log(snap.val());
+        //   });
     })
 
 
